30cac5765d2d0f92a91639361ff6fa8b51aace54
Daniel Yates <dsyates@live.co.uk>
2024-04-29 20:53:40 +0100

    created properties page

---
+++ src/app/properties/[id]/ReservationSidebar.tsx
@@ -0,0 +1,7 @@
+export default function ReservationSidebar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/app/properties/[id]/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import ReservationSidebar from "./ReservationSidebar";
+
+export default function PropertyDetailPage() {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6">
+      <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          className="object-cover w-full h-full"
+          alt="Beach house"
+        />
+      </div>
+
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">Property name</h1>
+
+          <span className="mb-6 block text-lg text-gray-600">
+            5 guests - 2 bedrooms - 1 bathroom
+          </span>
+
+          <hr />
+
+          <div className="py-6 flex items-center space-x-4">
+            <Image
+              src="/profile_pic_1.jpg"
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="The user name"
+            />
+
+            <p>
+              <strong>John Doe</strong> is your host
+            </p>
+          </div>
+
+          <hr />
+
+          <p className="mt-6 text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut viverra
+            non enim sit amet placerat.
+          </p>
+        </div>
+        <div>
+          <ReservationSidebar />
+        </div>
+      </div>
+    </main>
+  );
+}
